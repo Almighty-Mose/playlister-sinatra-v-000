7cec64405157b9711cb19278017b53ec4c7a9499
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-21 21:44:49 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,11 @@ class ArtistController < ApplicationController
 
   get '/artists' do
     @artist = Artist.all
-    erb :'/artists/index'
+    erb :'artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
   end
 end
