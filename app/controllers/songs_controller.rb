7d343c78b3c78a6955847b6193873dfc5fdeb8a7
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-21 21:31:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,16 +4,17 @@ class SongController < ApplicationController
   get '/songs' do
     @song = Song.all
 
-    erb :'/songs/index'
+    erb :'songs/index'
   end
 
   get '/songs/new' do
-    erb :'/songs/new'
+    erb :'songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    @genre = Genre.find_by_id(@song.song_genres.)
     binding.pry
-    erb :'/songs/show'
+    erb :'songs/show'
   end
 end
