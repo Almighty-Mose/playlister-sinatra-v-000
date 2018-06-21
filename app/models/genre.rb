9cd52051f92ebda25e97c2847dbc75f55b29214f
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-21 20:35:57 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,12 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :artists, through: :songs
   has_many :songs, through: :song_genres
+
+  def slug
+    name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    Genre.all.find{|artist| artist.slug == slug}
+  end
 end
