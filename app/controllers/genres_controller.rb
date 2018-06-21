8dd47b590b35735919f72d51823ad5406c025227
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-21 21:51:07 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,9 @@ class GenreController < ApplicationController
     @genre = Genre.all
     erb :'/genres/index'
   end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'genres/show'
+  end
 end
